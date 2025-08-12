3a15318375c43d1e72bd49f12fe86ae0930415a4
Lareciyo <lmcneal@perseverenow.org>
2025-08-12 16:32:56 -0500

    third comment

--- myApp.js
+++ myApp.js
@@ -1,15 +1,18 @@
 let express = require('express');
+const path = require('path')
 
 let app = express();
+
+
+
+app.get('/',  (req, res)=>{
+    htmlPath = __dirname +'/views/index.html';
+    res.sendFile(htmlPath)
+});
+
 app.listen(3000, () => {
      console.log("Hello World");
 })
-
-app.get('/', (req, res)=>{
-    res.send("Hello Express")
-});
-
-
    
 
 
