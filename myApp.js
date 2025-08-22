1b82127247ebf4e6668a1d85a9959ada1415b0ef
Lareciyo <lmcneal@perseverenow.org>
2025-08-22 10:20:37 -0500

    1020

--- myApp.js
+++ myApp.js
@@ -1,18 +1,12 @@
 require('dotenv').config()
-
 let express = require('express');
 const path = require('path')
-
 let app = express();
-
 app.use("/public", express.static(__dirname + "/public"));
-
 app.get("/json", (req, res) => {
  const messageStyle = process.env['MESSAGE_STYLE'];
 console.log('MESSAGE_STYLE:', messageStyle);
-
   let message = "Hello json";
-
   if (messageStyle === "uppercase") {
     message = message.toUpperCase();
   }
