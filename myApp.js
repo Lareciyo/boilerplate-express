f7c9cdb361b85e96cb56d4008556d5c5844306e1
Lareciyo <lmcneal@perseverenow.org>
2025-08-22 10:34:45 -0500

    1034

--- myApp.js
+++ myApp.js
@@ -6,14 +6,15 @@ app.use("/public", express.static(__dirname + "/public"));
 
 
 app.get("/json", (req, res) => {
-    let message;
-    if (process.env['MESSAGE_STYLE'] === "uppercase") {
-        message = "Hello json".toUpperCase();
-    }
-    else {
-        message = "Hello json";
-    }
-    res.json({ "message": `${message}` });
+  let message;
+  const style = process.env['MESSAGE_STYLE'];
+  console.log('MESSAGE_STYLE:', style); // debug
+  if (style === "uppercase") {
+    message = "Hello json".toUpperCase();
+  } else {
+    message = "Hello json";
+  }
+  res.json({ message });
 });
 
 
